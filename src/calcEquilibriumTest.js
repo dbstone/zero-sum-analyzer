4fc0f485ba24e0fb1a4a29f4d98da7315d9e0877
Devin Stone <bitq00@gmail.com>
2020-03-03 15:18:16 -0800

    giving up on SimplexJS, switching to pivot method

--- src/calcEquilibriumTest.js
+++ src/calcEquilibriumTest.js
@@ -1,41 +1,50 @@
 const calcEquilibrium = require('./calcEquilibrium')
 
-// rps_matrix = [
-//   [0, -1, 1],
-//   [1, 0, -1],
-//   [-1, 1, 0],
-// ]
+rps_matrix = [
+  [0, -1, 1],
+  [1, 0, -1],
+  [-1, 1, 0],
+]
 
-// console.log(calcEquilibrium(rps_matrix))
+console.log(calcEquilibrium(rps_matrix))
 
-// sfv_matrix = [
-//   [0, 200, -150],
-//   [120, 0, -150],
-//   [0, 0, 280],
-// ]
+sfv_matrix = [
+  [0, 200, -150],
+  [120, 0, -150],
+  [0, 0, 280],
+]
 
-// console.log(calcEquilibrium(sfv_matrix))
+console.log(calcEquilibrium(sfv_matrix))
 
-// sfv_matrix = [
-//   [0, -200, 150],
-//   [-120, 0, 150],
-//   [0, 0, -280],
-// ]
+sfv_matrix = [
+  [0, -200, 150],
+  [-120, 0, 150],
+  [0, 0, -280],
+]
 
-// console.log(calcEquilibrium(sfv_matrix))
+console.log(calcEquilibrium(sfv_matrix))
 
-// sfv_matrix = [
-//   [0, 120, 0],
-//   [200, 0, 0],
-//   [-150, -150, 280],
-// ]
+sfv_matrix = [
+  [0, 120, 0],
+  [200, 0, 0],
+  [-150, -150, 280],
+]
 
-// console.log(calcEquilibrium(sfv_matrix))
+console.log(calcEquilibrium(sfv_matrix))
 
 sfv_matrix = [
   [0, 0, 200, -150],
   [120, 0, 0, -150],
   [0, 200, 0, 280],
+]
+
+console.log(calcEquilibrium(sfv_matrix))
+
+sfv_matrix = [
+  [0, 120, 0],
+  [0, 0, 200],
+  [200, 0, 0],
+  [-150, -150, 280],
 ]
 
 console.log(calcEquilibrium(sfv_matrix))
