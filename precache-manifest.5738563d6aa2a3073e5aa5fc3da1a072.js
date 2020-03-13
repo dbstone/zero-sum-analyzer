9799702ce5be06ad1c04796d4b362242cae2355a
Devin Stone <bitq00@gmail.com>
2020-03-13 00:11:22 -0700

    Updates

---
+++ precache-manifest.5738563d6aa2a3073e5aa5fc3da1a072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ac490a7c0743d64d1a0865cf1e2b32",
+    "url": "/zero-sum-analyzer/index.html"
+  },
+  {
+    "revision": "7615b6963c1b76af4e48",
+    "url": "/zero-sum-analyzer/static/css/main.78d8fd9b.chunk.css"
+  },
+  {
+    "revision": "ba49ce931ecd7c0a2af8",
+    "url": "/zero-sum-analyzer/static/js/2.770c6d5e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/zero-sum-analyzer/static/js/2.770c6d5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7615b6963c1b76af4e48",
+    "url": "/zero-sum-analyzer/static/js/main.f20d2f46.chunk.js"
+  },
+  {
+    "revision": "08279526497bd2c6d68e",
+    "url": "/zero-sum-analyzer/static/js/runtime-main.27b99aa6.js"
+  }
+]);
